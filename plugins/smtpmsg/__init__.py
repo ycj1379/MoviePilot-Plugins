caaaa8525363982d6b4dc50aa8dbb4299e05151c
Aqr-K <95741669+Aqr-K@users.noreply.github.com>
2024-06-04 19:11:40 +0800

    Update ```SmtpMsg``` to ```v2.5.1```
    - 2.5.1 更新内容：
      - 修复：
        - 插件内版本号未更新，导致插件市场无法正常更新插件。

--- plugins/smtpmsg/__init__.py
+++ plugins/smtpmsg/__init__.py
@@ -88,7 +88,7 @@ class SmtpMsg(_PluginBase):
     # 插件图标
     plugin_icon = "Synomail_A.png"
     # 插件版本
-    plugin_version = "2.4"
+    plugin_version = "2.5.1"
     # 插件作者
     plugin_author = "Aqr-K"
     # 作者主页
