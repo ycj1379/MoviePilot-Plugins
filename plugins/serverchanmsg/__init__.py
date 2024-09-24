4a77a1e1b45622d064f8269455e61a4d49ba07a0
Aqr-K <95741669+Aqr-K@users.noreply.github.com>
2024-09-24 14:53:05 +0800

    Update ```ServerChanMsg``` to ```v0.3```
    - 1.2 更新内容：
      - 增加：
        - 支持v2.0+使用。

--- plugins/serverchanmsg/__init__.py
+++ plugins/serverchanmsg/__init__.py
@@ -25,7 +25,7 @@ class ServerChanMsg(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/Aqr-K/MoviePilot-Plugins/main/icons/ServerChan.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "Aqr-K"
     # 作者主页
