6dbdca78fbe60d338db14e0147789717131daefe
ycj1379 <ycj1379@gmail.com>
2024-09-30 10:08:13 +0800

    Update __init__.py

--- plugins/serverchan3msg/__init__.py
+++ plugins/serverchan3msg/__init__.py
@@ -17,7 +17,7 @@ import threading
 lock = threading.Lock()
 
 
-class ServerChanMsg(_PluginBase):
+class ServerChan3Msg(_PluginBase):
     # 插件名称
     plugin_name = "Server酱3消息通知"
     # 插件描述
@@ -31,7 +31,7 @@ class ServerChanMsg(_PluginBase):
     # 作者主页
     author_url = "https://github.com/Aqr-K"
     # 插件配置项ID前缀
-    plugin_config_prefix = "serverchanmsg_"
+    plugin_config_prefix = "serverchan3msg_"
     # 加载顺序
     plugin_order = 31
     # 可使用的用户级别
